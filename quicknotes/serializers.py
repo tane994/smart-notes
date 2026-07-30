1f2c45f739a8bceff35370c64d00cf37ed5f5ced
atanesini <Andres.Tanesini@lts.it>
2026-07-30 15:09:17 +0200

    feat: setup pagination, serializers and jwt

--- quicknotes/serializers.py
+++ quicknotes/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework.serializers import ModelSerializer
-from quicknotes.models import Note
+from rest_framework import serializers
+from quicknotes.models import Note, Collection
+
+class CollectionSerializer(ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = '__all__'
+        
 
 class NoteSerializer(ModelSerializer):
+    collection_data = CollectionSerializer(source="collection", read_only=True)
+
     class Meta:
         model = Note
-        fields = ['id', 'title', 'content']
+        fields = "__all__"
+
+class CollectionWithNotesSerializer(ModelSerializer):
+    notes = NoteSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Collection
+        fields = "__all__"
--- quicknotes/views.py
+++ quicknotes/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse, JsonResponse
-from quicknotes.models import Note
+from quicknotes.models import Note, Collection
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
-from quicknotes.serializers import NoteSerializer
+from quicknotes.serializers import CollectionWithNotesSerializer, NoteSerializer, CollectionSerializer
+#from django.db import connection
+from rest_framework.decorators import action
 
 def home(request):
     return HttpResponse('Welcome Home')
@@ -15,10 +17,19 @@ class NoteViewSet(ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
     
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.get_serializer(queryset, many=True)
-        return Response({'data': serializer.data})
+    def get_queryset(self):
+        queryset = Note.objects.select_related('collection')
+        collection_id = self.request.query_params.get('collection_id')
+        if collection_id:
+            queryset = queryset.filter(collection_id=collection_id)
+        return queryset.order_by('id')
+    
+    # def list(self, request, *args, **kwargs):
+    #     queryset = self.filter_queryset(self.get_queryset())
+    #     serializer = self.get_serializer(queryset, many=True)
+    #     data = serializer.data
+    #     # print(len(connection.queries))
+    #     return Response({'data': data})
     
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -27,3 +38,25 @@ class NoteViewSet(ModelViewSet):
     
     
     
+class CollectionViewSet(ModelViewSet):
+    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({"data": serializer.data})
+    
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response({"data": serializer.data})
+
+    @action(detail=True, methods=['GET'])
+    def notes(self, request, pk=None):
+        collection = Collection.objects.prefetch_related('notes').get(pk=pk)
+        # serializer = CollectionSerializer(collection)
+        # serializer_notes = NoteSerializer(collection.notes, many=True)
+        # return Response({'data': {**dict(serializer.data), 'notes':serializer_notes.data}})
+        serializer = CollectionWithNotesSerializer(collection)
+        return Response({'data': serializer.data})
